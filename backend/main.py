7e630cebbe28979697cac11d03300d5edd5d2dcc
Romasha Guin <romashaguin@umass.edu>
2026-06-13 19:38:12 +0530

    Add Vercel domain to CORS

--- backend/main.py
+++ backend/main.py
@@ -33,6 +33,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://skillio-two.vercel.app",
         "https://*.vercel.app",
     ],
     allow_credentials=True,
